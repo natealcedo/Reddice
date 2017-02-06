0e7a2a9c0fac065058d935ce13eaff2c7008c111
Nate <nathanielalcedojr@gmail.com>
2017-02-07 00:09:05 +0800

    Lesson 5: User Sign Up: Sign Up form and its state

--- client/index.js
+++ client/index.js
@@ -3,4 +3,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
-render(<Router history={browserHistory} routes={routes}/>, document.getElementById('app'));
+render(
+	<Router history={browserHistory} routes={routes} />,
+	document.getElementById('app')
+);
