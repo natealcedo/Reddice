aec55d8fe9525d8f05a46a42f2c8fa1bcf93f331
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-09 20:46:08 +0800

    Lesson 12: User sign up: Save user in DB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import validateInput from '../shared/validations/signup';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user';
 
 const router = express.Router();
 
@@ -7,7 +10,18 @@ router.post('/', (req, res) => {
 	const {errors, isValid} = validateInput(req.body);
 
 	if (isValid) {
-		res.json({ success: true });
+		const {username, password, timezone, email} = req.body;
+		const password_digest = bcrypt.hashSync(password, 10);
+		console.log('valid');
+		User.forge({
+			username, timezone, email, password_digest
+		}, { hasTimeStamps: true })
+			.save()
+			.then(user => res.json({ success: true }))
+			.catch(err => {
+				console.log(err);
+				res.status(500).json({ error: err });
+			});
 	} else {
 		res.status(400).json(errors);
 	}
