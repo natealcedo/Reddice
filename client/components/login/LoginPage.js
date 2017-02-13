b0b5c0e7ee70215ef8dcbcfbe81406444c03fa3f
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-13 22:39:15 +0800

    Lesson 15: Authentication: Login Form

---
+++ client/components/login/LoginPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LoginForm from './LoginForm';
+
+class LoginPage extends Component {
+	render() {
+		return (
+			<div className='row'>
+				<div className="col-md cold-md-offset-4">
+					<LoginForm />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default LoginPage;
