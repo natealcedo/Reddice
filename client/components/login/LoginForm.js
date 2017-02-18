c21a52fa99208e0d0d0a9fcd101bec23f64e3c3d
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-18 14:12:28 +0800

    Lesson 16: Authentication: Verify User

--- client/components/login/LoginForm.js
+++ client/components/login/LoginForm.js
@@ -5,83 +5,85 @@ import { connect } from 'react-redux';
 import { login } from '../../actions/login';
 
 class LoginForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			identifier: '',
-			password: '',
-			errors: {},
-			isLoading: false
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            identifier: '',
+            password: '',
+            errors: {},
+            isLoading: false
+        };
 
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
-	isValid() {
-		const {errors, isValid} = validateInput(this.state);
+    isValid() {
+        const {errors, isValid} = validateInput(this.state);
 
-		if (!isValid) {
-			this.setState({ errors });
-		}
+        if (!isValid) {
+            this.setState({ errors });
+        }
 
-		return isValid;
-	}
+        return isValid;
+    }
 
-	onSubmit(e) {
-		e.preventDefault();
-		if (this.isValid()) {
-			this.setState({ errors: {}, isLoading: true });
-			this.props.login(this.state).then(
-				res => this.context.router.push('/'),
-				err => this.setState({ errors: err.data.errors, isLoading: false })
-			);
-		}
-	}
+    onSubmit(e) {
+        e.preventDefault();
+        if (this.isValid()) {
+            this.setState({ errors: {}, isLoading: true });
+            this.props.login(this.state).then(
+                res => this.context.router.push('/'),
+                err => this.setState({ errors: err.data.errors, isLoading: false })
+            );
+        }
+    }
 
-	onChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
-	}
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
 
 
-	render() {
-		const {errors, identifier, password, isLoading} = this.state;
+    render() {
+        const {errors, identifier, password, isLoading} = this.state;
 
-		return (
-			<form onSubmit={this.onSubmit}>
-				<h1>Login</h1>
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Login</h1>
 
-				<TextFieldGroup
-					field='identifier'
-					label='Username / Email'
-					value={identifier}
-					error={errors.identifier}
-					onChange={this.onChange}
-				/>
+                { errors.form && <div className="alert alert-danger">{errors.form}</div> }
 
-				<TextFieldGroup
-					field='password'
-					label='Password'
-					value={password}
-					error={errors.password}
-					onChange={this.onChange}
-					type='password'
-				/>
+                <TextFieldGroup
+                    field='identifier'
+                    label='Username / Email'
+                    value={identifier}
+                    error={errors.identifier}
+                    onChange={this.onChange}
+                />
 
-				<div className="form-group">
-					<button className="btn btn-primary btn-lg" disabled={isLoading}>Login</button>
-				</div>
-			</form>
-		);
-	}
+            <TextFieldGroup
+                field='password'
+                label='Password'
+                value={password}
+                error={errors.password}
+                onChange={this.onChange}
+                type='password'
+            />
+
+            <div className="form-group">
+                <button className="btn btn-primary btn-lg" disabled={isLoading}>Login</button>
+            </div>
+        </form>
+        );
+    }
 }
 
 LoginForm.propTypes = {
-	login: PropTypes.func.isRequired,
+    login: PropTypes.func.isRequired,
 };
 
 LoginForm.contextTypes = {
-	router: PropTypes.object.isRequired
+    router: PropTypes.object.isRequired
 };
 
 export default connect(null, { login })(LoginForm);
