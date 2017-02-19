1351df75e35736480b581708e32388a839412f54
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-19 15:54:32 +0800

    Lesson 19 Authentication: Verify JWT on Server with Express MiddleWare

---
+++ client/components/events/NewEventPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EventForm from './EventForm';
+
+class NewEventPage extends React.Component {
+    render() {
+        return (
+            <EventForm />
+        );
+    }
+}
+
+export default NewEventPage;
